4703a76ee3d0d268537cfe565165f3058ca87a96
KARAMOKO1 <karamokoj19@gmail.com>
2026-09-24 01:20:57 +0000

    Update CACHE_NAME to version v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kara-pronos-v12';
+const CACHE_NAME = 'kara-pronos-v14';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
