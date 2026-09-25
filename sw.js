a55c620ffdf185ae22c6146218e05ae5b9e5a05f
KARAMOKO1 <karamokoj19@gmail.com>
2026-09-25 01:42:01 +0000

    Update CACHE_NAME to version v30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kara-pronos-v29';
+const CACHE_NAME = 'kara-pronos-v30';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
